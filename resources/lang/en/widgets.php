7be5b02681cf88dbb2c30d14b3f229bdc996037a
Bezhan Salleh <bezhan_salleh@yahoo.com>
2022-03-01 05:26:04 +0430

    added the ability translate

---
+++ resources/lang/en/widgets.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    /**
+     * Dashboard Page
+     */
+    'navigation_label' => 'Google Analytics Dashboard',
+    'title' => 'Google Analytics Dashboard',
+
+    /**
+     * Widget Heading/Labels
+     */
+    'page_views' => 'Page Views',
+    'visitors' => 'Unique Users',
+    'sessions' => 'Sessions',
+    'sessions_duration' => 'Avg. Sessions Duration',
+    'sessions_by_country' => 'Sessions By Country',
+    'sessions_by_device' => 'Sessions By Device',
+    'top_referrers' => 'Top Referrers',
+    'most_visited_pages' => 'Most Visited Pages',
+    'one_day_active_users' => '1 Day Active Users',
+    'seven_day_active_users' => '7 Day Active Users',
+    'fourteen_day_active_users' => '1 Day Active Users',
+    'twenty_eight_day_active_users' => '1 Day Active Users',
+
+    /**
+     * Filter Labels
+     */
+    'T' => 'Today',
+    'Y' => 'Yesterday',
+    'TW' => 'This Week',
+    'LW' => 'Last Week',
+    'LM' => 'Last Month',
+    'TM' => 'This Month',
+    'TY' => 'This Year',
+    'LSD' => 'Last 7 Days',
+    'LTD' => 'Last 30 Days',
+    'FD' => '5 Days',
+    'TD' => '10 Days',
+    'FFD' => '15 Days',
+
+    /**
+     * Trajectory Label Descriptions
+     */
+    'trending_up' => 'Increase',
+    'trending_down' => 'Decrease',
+    'steady' => 'Steady',
+];
