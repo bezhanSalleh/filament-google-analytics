a4e28f520561e4b3936e4f6a6fb4ade558917a21
Saifallak <Saifallak@yahoo.com>
2023-07-04 17:10:50 +0300

    hot fix for 0 not found.

--- src/Traits/Visitors.php
+++ src/Traits/Visitors.php
@@ -19,8 +19,8 @@ trait Visitors
         );
 
         return [
-            'result' => $analyticsData[0]['activeUsers'],
-            'previous' => $analyticsData[1]['activeUsers']
+            'previous' => $analyticsData->first()['activeUsers'] ?? 0,
+            'result' => $analyticsData->last()['activeUsers'] ?? 0,
         ];
     }
 
@@ -33,8 +33,8 @@ trait Visitors
         );
 
         return [
-            'result' => $analyticsData[0]['activeUsers'],
-            'previous' => $analyticsData[1]['activeUsers']
+            'previous' => $analyticsData->first()['activeUsers'] ?? 0,
+            'result' => $analyticsData->last()['activeUsers'] ?? 0,
         ];
     }
 
