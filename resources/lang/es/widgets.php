ccea3d3724a2e065348ea62dbe48c647c0d98206
Olivtradet <deoliveira.elias.eus@gmail.com>
2024-06-07 17:16:15 +0200

    added Spanish translations

---
+++ resources/lang/es/widgets.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    /**
+     * Dashboard Page
+     */
+    'navigation_label' => 'Panel de control de Google Analytics',
+    'title' => 'Panel de Google Analytics',
+
+    /**
+     * Widget Heading/Labels
+     */
+    'page_views' => 'Páginas vistas',
+    'visitors' => 'Usuarios Únicos',
+    'sessions' => 'Sesiones',
+    'sessions_duration' => 'Duración media. Duración Sesiones',
+    'sessions_by_country' => 'Sesiones por país',
+    'sessions_by_device' => 'Sesiones por dispositivo',
+    'top_referrers' => 'Principales remitentes',
+    'most_visited_pages' => 'Páginas más visitadas',
+    'one_day_active_users' => 'Usuarios activos 1 día',
+    'seven_day_active_users' => 'Usuarios Activos de 7 Días',
+    'fourteen_day_active_users' => 'Usuarios Activos 14 Días',
+    'twenty_eight_day_active_users' => 'Usuarios activos 28 días',
+
+    /**
+     * Filter Labels
+     */
+    'T' => 'Hoy',
+    'Y' => 'Ayer',
+    'TW' => 'Esta Semana',
+    'LW' => 'Última semana',
+    'LM' => 'Mes pasado',
+    'TM' => 'Este Mes',
+    'TY' => 'Este año',
+    'LSD' => 'Últimos 7 días',
+    'LTD' => 'Últimos 30 días',
+    'FD' => '5 Días',
+    'TD' => '10 Días',
+    'FFD' => '15 días',
+
+    /**
+     * Trajectory Label Descriptions
+     */
+    'trending_up' => 'Aumento',
+    'trending_down' => 'Disminución',
+    'steady' => 'Constante',
+];
