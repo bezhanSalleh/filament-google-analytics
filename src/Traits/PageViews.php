a2a6ccdcee9b54d4fce3c4baccb241a705b585b6
Bezhan Salleh <bezhan_salleh@yahoo.com>
2024-10-15 04:24:58 +0200

    fixes Pages views result for today/yesterday

--- src/Traits/PageViews.php
+++ src/Traits/PageViews.php
@@ -15,8 +15,8 @@ trait PageViews
         $analyticsData = Analytics::fetchTotalVisitorsAndPageViews(Period::days(1));
 
         return [
-            'result' => $analyticsData->last()['screenPageViews'] ?? 0,
-            'previous' => $analyticsData->first()['screenPageViews'] ?? 0,
+            'result' => $analyticsData->first()['screenPageViews'] ?? 0,
+            'previous' => $analyticsData->last()['screenPageViews'] ?? 0,
         ];
     }
 
@@ -25,14 +25,15 @@ trait PageViews
         $analyticsData = Analytics::fetchTotalVisitorsAndPageViews(Period::create(Carbon::yesterday()->clone()->subDay(), Carbon::yesterday()));
 
         return [
-            'result' => $analyticsData->last()['screenPageViews'] ?? 0,
-            'previous' => $analyticsData->first()['screenPageViews'] ?? 0,
+            'result' => $analyticsData->first()['screenPageViews'] ?? 0,
+            'previous' => $analyticsData->last()['screenPageViews'] ?? 0,
         ];
     }
 
     private function pageViewsLastWeek(): array
     {
         $lastWeek = $this->getLastWeek();
+
         $currentResults = $this->get('screenPageViews', 'year', $lastWeek['current']);
         $previousResults = $this->get('screenPageViews', 'year', $lastWeek['previous']);
 
