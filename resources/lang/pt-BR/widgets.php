24bce8cf2aeee741cefd7f9283d380a8def7cf36
a21ns1g4ts <atila.danvi@outlook.com>
2024-11-16 15:13:09 -0300

    Add pt translations

---
+++ resources/lang/pt-BR/widgets.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+    /**
+     * Dashboard Page
+     */
+    'navigation_label' => 'Painel do Google Analytics',
+    'title' => 'Painel do Google Analytics',
+
+     /**
+     * Widget Heading/Labels
+     */
+    'page_views' => 'Visualizações de Página',
+    'visitors' => 'Usuários Únicos',
+    'sessions' => 'Sessões',
+    'sessions_duration' => 'Duração Média das Sessões',
+    'sessions_by_country' => 'Sessões por País',
+    'sessions_by_device' => 'Sessões por Dispositivo',
+    'top_referrers' => 'Principais Referenciadores',
+    'most_visited_pages' => 'Páginas Mais Visitadas',
+    'one_day_active_users' => 'Usuários Ativos em 1 Dia',
+    'seven_day_active_users' => 'Usuários Ativos em 7 Dias',
+    'fourteen_day_active_users' => 'Usuários Ativos em 14 Dias',
+    'twenty_eight_day_active_users' => 'Usuários Ativos em 28 Dias',
+
+    /**
+     * Filter Labels
+     */
+    'T' => 'Hoje',
+    'Y' => 'Ontem',
+    'TW' => 'Esta Semana',
+    'LW' => 'Semana Passada',
+    'LM' => 'Mês Passado',
+    'TM' => 'Este Mês',
+    'TY' => 'Este Ano',
+    'LSD' => 'Últimos 7 Dias',
+    'LTD' => 'Últimos 30 Dias',
+    'FD' => '5 Dias',
+    'TD' => '10 Dias',
+    'FFD' => '15 Dias',
+
+    /**
+     * Trajectory Label Descriptions
+     */
+    'trending_up' => 'Aumento',
+    'trending_down' => 'Diminuição',
+    'steady' => 'Estável',
+];
